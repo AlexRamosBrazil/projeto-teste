14750b84b1b255497d11b23856ea42d34fe637c7
AlexRamosBrazil <eng.alexramos@gmail.com>
2024-05-08 15:07:52 -0300

    desafio felipao testado e funcional

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 let nome = "Alex"
+let experience = 12000
+let nivel = null
 
-console.log("Hello " + nome)
+if (experience <= 1000) {
+    nivel = "Ferro"
+}else if (experience <= 2000) {
+    nivel = "Bronze"
+}else if (experience <= 5000) {
+    nivel = "Prata"
+}else if (experience <= 7000) {
+    nivel = "Ouro"
+}else if (experience <= 8000) {
+    nivel = "Platina"
+}else if (experience <= 9000) {
+    nivel = "Ascendente"
+}else if (experience <= 10000) {
+    nivel = "Imortal"
+}else {
+    nivel = "Radiante"
+}
+
+console.log("O Herói de nome " + nome + " está no nível de " + nivel)
+
+
